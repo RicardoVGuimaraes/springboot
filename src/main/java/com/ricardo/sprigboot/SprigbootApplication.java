e287a3bc572ea78afec3d26fe59e7ece0e941000
Ricardo <ricardo_vicenteguimaraes@yahoo.com.br>
2022-03-26 22:39:50 -0300

    Created project.

---
+++ src/main/java/com/ricardo/sprigboot/SprigbootApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.sprigboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/sprigboot/SprigbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.sprigboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
